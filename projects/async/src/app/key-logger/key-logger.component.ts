0f0c2b1c2ff055fd8b817b645df262c7886e34e3
onowakow <nowakowskiowen08@gmail.com>
2022-02-16 16:10:12 -0700

    Key logger pipe example added

--- projects/async/src/app/key-logger/key-logger.component.ts
+++ projects/async/src/app/key-logger/key-logger.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-key-logger',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./key-logger.component.css']
 })
 export class KeyLoggerComponent implements OnInit {
+  keys: string = '';
+
+  @ViewChild('keyContainer', {static: true}) input!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    const logger = fromEvent(this.input.nativeElement, 'keyup');
+    logger.pipe(
+      tap((evt: any) => {
+      this.keys += evt.key;
+    })).subscribe()
   }
 
 }
