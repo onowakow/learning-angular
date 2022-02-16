b34bbd095e7c2d993403b94d8525177641938fb7
onowakow <nowakowskiowen08@gmail.com>
2022-02-16 15:35:38 -0700

    added keyUp component

--- projects/async/src/app/app.module.ts
+++ projects/async/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { KeyUpComponent } from './key-up/key-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeyUpComponent
   ],
   imports: [
     BrowserModule
---
+++ projects/async/src/app/key-up/key-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-key-up',
+  templateUrl: './key-up.component.html',
+  styleUrls: ['./key-up.component.css']
+})
+export class KeyUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
